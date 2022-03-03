8078b81f80db1e512c74e4f3c36ff5279b1b778f
clairesheek <clairesheek@gmail.com>
2022-03-03 11:56:13 -0600

    heroku deploy

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Animated } from "react-animated-css";
 import Pdf from "./Pdf";
 import ClaireSheekResume from "../images/ClaireSheekResume.pdf";
+import download from "../images/download.png";
 
 const Resume = () => {
   return (
@@ -10,12 +11,18 @@ const Resume = () => {
         <h2 className="title">Resume</h2>
       </Animated>
       <div className="content resume">
+        <a
+          href={ClaireSheekResume}
+          download="ClaireSheekResume.pdf"
+          style={{ padding: "16px" }}
+        >
+          Download
+          <img
+            src={download}
+            style={{ paddingLeft: "6px", maxHeight: "12px" }}
+          ></img>
+        </a>
         <Pdf />
-        <button>
-          <a href={ClaireSheekResume} download="ClaireSheekResume.pdf">
-            Download
-          </a>
-        </button>
       </div>
     </Animated>
   );
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,39 +1,81 @@
-import React from 'react'
-import {Animated} from "react-animated-css";
-import { Link } from 'react-router-dom'
-import Linkedin from '../images/Linkedin'
-import Github from '../images/Github'
-import Gmail from '../images/Gmail'
+import React from "react";
+import { Animated } from "react-animated-css";
+import { NavLink } from "react-router-dom";
+import Linkedin from "../images/Linkedin";
+import Github from "../images/Github";
+import Gmail from "../images/Gmail";
 
-const SideNav = ({page, setPage}) => {
-
+const SideNav = ({ page, setPage }) => {
   return (
     <nav>
-      <Animated animationIn="fadeInDown" >
-      <div className="">
-        <Link to={'/'} name="Landing">
-          <h2>Claire Sheek</h2>
-          <h4>Full Stack Developer</h4>
-        </Link>
-      </div>
+      <Animated animationIn="fadeInDown">
+        <div className="">
+          <NavLink to={"/"} name="Landing">
+            <h2>Claire Sheek</h2>
+            <h4>Full Stack Developer</h4>
+          </NavLink>
+        </div>
       </Animated>
       <Animated animationIn="slideInLeft">
-        <hr/>
+        <hr />
       </Animated>
       <Animated animationIn="slideInLeft" animationInDelay={100}>
-      <ol>
-        <li><Link to={'/portfolio'} name="Portfolio">Portfolio</Link></li>      {/* </Animated> */}
-        <li><Link to={'/resume'} name="Resume">Resume</Link></li>
-        <li><Link to={'/contact'} name="Contact">Contact</Link></li>
-      </ol>
+        <ol>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navItem")}
+              to={"/portfolio"}
+              name="Portfolio"
+            >
+              Portfolio
+            </NavLink>
+          </li>{" "}
+          {/* </Animated> */}
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navItem")}
+              to={"/resume"}
+              name="Resume"
+            >
+              Resume
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "navItem")}
+              to={"/contact"}
+              name="Contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ol>
         <div>
-          <a href={"https://www.linkedin.com/in/clairesheek"} target="_blank" rel="noreferrer"><Linkedin className="icon"/></a>
-          <a href={"https://www.github.com/ClaireSheek"} target="_blank" rel="noreferrer"><Github className="icon"/></a>
-          <a href={"mailto: clairesheek@gmail.com"} target="_blank" rel="noreferrer"><Gmail className="icon"/></a>
+          <a
+            href={"https://www.linkedin.com/in/clairesheek"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Linkedin className="icon" />
+          </a>
+          <a
+            href={"https://www.github.com/ClaireSheek"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github className="icon" />
+          </a>
+          <a
+            href={"mailto: clairesheek@gmail.com"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Gmail className="icon" />
+          </a>
         </div>
       </Animated>
     </nav>
-  )
-}
+  );
+};
 
-export default SideNav
+export default SideNav;
